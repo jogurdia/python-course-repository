c43462740d949aad150d0fbb706e3227880c08d0
Johan Gurdian <jogurdia@cisco.com>
2020-01-26 21:11:08 -0600

    Data types

---
+++ data-type.py
@@ -0,0 +1,38 @@
+#Strings
+
+print("Hello World")
+print('Hello world')
+print("""Hello world""")
+
+# Integer
+
+print(10)
+print(20)
+
+# Float
+
+print(2.5)
+print(10.4)
+
+# Boolean
+
+True
+False
+
+# List
+
+print([10,20,30])
+print(['Hello', 'Bye', 'Lista'])
+print(10, 'Lista', False, 10.5)
+[]
+# Tuples
+
+print(1,6)
+()
+
+# Dictionaries 
+
+print({
+    "Name" : "Johan",
+    "Last name" : "Gurdian"
+})
