deeb0273c974f57a1974b43ecef621b895454b7f
Johan Gurdian <jogurdia@cisco.com>
2020-01-26 21:44:30 -0600

    Commit variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+# name of variable = data type
+
+name = "Johan"
+
+print(name)
+
+x = 100
+book = "Dracula"
+
+print(x)
+print(book)
+
+# Variable names cannot start with numbers
+# Variables names should be something easy to understand
+
+# Name conventions
+
+book_name = "Dracula" #Snake Case
+bookName = "Dracula"  #Camel Case
+BookName = "Dracula"  #Pascal Case
+
+# Contant is a valor that we dont want to change, in that case we can use variable name is CAP
+
+MY_NAME = "Johan"
+
